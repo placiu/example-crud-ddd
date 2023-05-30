845ea5dc18d574d482f956e15fe9b1c5b992edf7
placiu <placiu@gmail.com>
2023-05-30 20:30:50 +0200

    validate inputs & display errors

--- resources/js/Components/FormAdd.vue
+++ resources/js/Components/FormAdd.vue
@@ -1,7 +1,6 @@
 <script setup>
     import { useForm } from '@inertiajs/vue3'
-    import { router } from '@inertiajs/vue3'
-
+  
     const form = useForm({
         make: null,
         model: null,
@@ -9,8 +8,10 @@
     })
 
     function add() {
-        router.post('/cars', form);
-        form.reset();
+        form.post('/cars', {
+            preserveScroll: true,
+            onSuccess: () => form.reset(),
+        });
     }
 </script>
 
@@ -20,18 +21,21 @@
             <label for="make" class="block text-sm font-medium leading-6 text-gray-900">Marka</label>
             <div class="mt-2">
               <input v-model="form.make" type="text" name="make" id="make" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" />
+              <div class="mt-2 text-xs text-red-600" v-if="form.errors.make">{{ form.errors.make }}</div>
             </div>
         </div>
         <div class="col-span-full">
             <label for="model" class="block text-sm font-medium leading-6 text-gray-900">Model</label>
             <div class="mt-2">
                 <input v-model="form.model" type="text" name="model" id="model" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" />
+                <div class="mt-2 text-xs text-red-600" v-if="form.errors.model">{{ form.errors.model }}</div>
             </div>
         </div>
         <div class="col-span-full">
             <label for="licence-number" class="block text-sm font-medium leading-6 text-gray-900">Nr Rejestracyjny</label>
             <div class="mt-2">
                 <input v-model="form.licence_number" type="text" name="licence_number" id="licence-number" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" />
+                <div class="mt-2 text-xs text-red-600" v-if="form.errors.licence_number">{{ form.errors.licence_number }}</div>
             </div>
         </div>
         <button type="submit" class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Save</button>
--- resources/js/Components/FormEdit.vue
+++ resources/js/Components/FormEdit.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { useForm } from '@inertiajs/vue3'
-import { router } from '@inertiajs/vue3'
 
     const props = defineProps({
         car: Object,
@@ -13,7 +12,9 @@ import { router } from '@inertiajs/vue3'
     })
 
     function update() {
-        router.put('/cars/' + props.car.id, form);
+        form.put('/cars/' + props.car.id, {
+            preserveScroll: true,
+        });
     }
 </script>
 
@@ -23,18 +24,21 @@ import { router } from '@inertiajs/vue3'
             <label for="make" class="block text-sm font-medium leading-6 text-gray-900">Marka</label>
             <div class="mt-2">
               <input type="text" name="make" v-model="form.make" id="make" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" />
+              <div class="mt-2 text-xs text-red-600" v-if="form.errors.make">{{ form.errors.make }}</div>
             </div>
         </div>
         <div class="col-span-full">
             <label for="model" class="block text-sm font-medium leading-6 text-gray-900">Model</label>
             <div class="mt-2">
                 <input type="text" name="model" v-model="form.model" id="model" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" />
+                <div class="mt-2 text-xs text-red-600" v-if="form.errors.model">{{ form.errors.model }}</div>
             </div>
         </div>
         <div class="col-span-full">
             <label for="licence-number" class="block text-sm font-medium leading-6 text-gray-900">Nr Rejestracyjny</label>
             <div class="mt-2">
                 <input type="text" name="licence_number" v-model="form.licence_number" id="licence-number" class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6" />
+                <div class="mt-2 text-xs text-red-600" v-if="form.errors.licence_number">{{ form.errors.licence_number }}</div>
             </div>
         </div>
         <button type="submit" class="rounded-md bg-gray-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">Save</button>
